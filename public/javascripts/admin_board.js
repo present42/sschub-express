e1ae2dbe737fa5a05aec615317f0c87ed5caf90a
huppanup <94790989+huppanup@users.noreply.github.com>
2022-07-31 23:16:17 +0900

    Added hover when delete is activated

--- public/javascripts/admin_board.js
+++ public/javascripts/admin_board.js
@@ -1,5 +1,17 @@
 var btn_delete = document.querySelector("#delete");
 
+
 btn_delete.addEventListener("click", function(e) {
    this.classList.toggle("is-active"); 
+   console.log(document.querySelectorAll('.board'));
+   document.querySelectorAll('.board').forEach(function(i){
+    console.log(i);
+     i.classList.toggle("select");
+   });
+   document.querySelectorAll('.thumbnail').forEach(function(i){
+    i.disabled = !i.disabled;
+   });
+   document.querySelectorAll('.img-inside-button').forEach(function(i){
+    i.disabled = !i.disabled;
+   });
 });
