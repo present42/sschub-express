0904ba8aaf71f3a590baf1636c1afdccebff3ffc
huppanup <94790989+huppanup@users.noreply.github.com>
2022-08-01 18:06:33 +0900

    New button-cancel delete

--- public/javascripts/admin_board.js
+++ public/javascripts/admin_board.js
@@ -1,17 +1,44 @@
 var btn_delete = document.querySelector("#delete");
+var board = document.querySelectorAll('.board');
 
+/* OnClick event handler for every board */
+
+board.forEach(function(i){
+    i.addEventListener("click", e => {
+        console.log("Board ID ", e.target.id, " was clicked");
+    });
+});
+
+/* Funtion for deleting boards */
+
+function deleteBoards(){
+    if (confirm("Are you sure you want to delete these boards?")){
+        /* Collect selected boards */
+        /* Send selected board id to route */
+        console.log("Delete boards.");
+    }
+}
+
+/* Event handler for delete button */
 
 btn_delete.addEventListener("click", function(e) {
-   this.classList.toggle("is-active"); 
-   console.log(document.querySelectorAll('.board'));
-   document.querySelectorAll('.board').forEach(function(i){
-    console.log(i);
-     i.classList.toggle("select");
+    /* Delete selected boards when delete button is-active */
+   this.classList.forEach(function(i){
+     if (i == 'is-active'){
+        deleteBoards();
+     }
    });
+   /* Activates/Deactivates delete button */
+   this.classList.toggle("is-active");
+   document.querySelector('.floating-container').classList.toggle("is-active");
+   /* Activates/Deactivates selection for each boards */
+   board.forEach(function(i){
+        i.classList.toggle("select");  
+   });
+
+   /* Ables/Disables redirection to editing page */
    document.querySelectorAll('.thumbnail').forEach(function(i){
     i.disabled = !i.disabled;
    });
-   document.querySelectorAll('.img-inside-button').forEach(function(i){
-    i.disabled = !i.disabled;
-   });
 });
+
