7a7919c0b6821b9d77675fa5b803a1137c81eba4
hkimbd <99864124+hkimbd@users.noreply.github.com>
2022-08-08 03:15:19 +0900

    form word limit

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -29,6 +29,27 @@ window.onload = (event) => {
   imgInp.onchange = (evt) => {
     myModal.show();
   };
+  
+  document.querySelectorAll('textarea[data-max-words]').forEach(input => {
+    // Remember the word limit for the current input
+    let maxWords = parseInt(input.getAttribute('data-max-words') || 0)
+    // Add an eventlistener to test for key inputs
+    input.addEventListener('keydown', e => {
+      let target = e.currentTarget
+      // Split the text in the input and get the current number of words
+      let words = target.value.split(/\s+/).length
+      // If the word count is > than the max amount and a space is pressed
+      // Don't allow for the space to be inserted
+      if (!target.getAttribute('data-announce'))
+        // Note: this is a shorthand if statement
+        // If the first two tests fail allow the key to be inserted
+        // Otherwise we prevent the default from happening
+        words >= maxWords && e.keyCode == 32 && e.preventDefault()
+      else
+        words >= maxWords && e.keyCode == 32 && (e.preventDefault() || alert('Word Limit Reached'))
+    })
+  })
+
 
   let crop_modal = document.getElementById("cropImageModal");
   crop_modal.addEventListener('shown.bs.modal', (evt) => {
