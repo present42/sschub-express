27d386582b2a35cf2fb5038b1be55e619fc5de95
huppanup <94790989+huppanup@users.noreply.github.com>
2022-10-10 01:02:28 +0800

    Guideline updates

--- database.cjs
+++ database.cjs
@@ -5,7 +5,12 @@ var conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'password',
+    // database: 'hello',
+    charset: 'utf8mb4'
 });
 
 conn.connect(function(err) {
