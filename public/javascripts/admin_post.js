49fc72e79997897f7a67ffea9a9ad76c5b8576ef
huppanup <94790989+huppanup@users.noreply.github.com>
2022-12-20 22:14:53 +0900

    Improved msg preview

--- public/javascripts/admin_post.js
+++ public/javascripts/admin_post.js
@@ -5,108 +5,79 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createNewMessage(size = 'md', message=". This content is a little bit longer.", nickname="hyunju", email="hyunju@connect.abc.com", image_path) {
-    color_list = ["rgba()", "#eee4da", "#edc22e"];
-    color = getRandomInt(0, 2);
-    color = color_list[color];
-    
-    console.log(size);
-      var height = 16,
-          width = 16,
-          top = 2;
-      if (size == 'bg') {
-          height = 25;
-          width = 25;
-          // top: 2 - 73
-          top = getRandomInt(2, 73);
-      } else if (size == 'md') {
-          height = 20;
-          width = 20;
-          // top: 2 - 79
-          top = getRandomInt(2, 79);
-      } else {
-          size = "sm";
-          // top: 2 - 82
-          top = getRandomInt(2, 82);
-      }
+function createNewMessage(id, size = 'md', message = "This content is a little bit longer.", nickname = "hyunju", filename, time) {
+  // color_list = ["rgba(255, 255, 255, 0.856)", "rgba(238, 228, 218, 0.87)", "rgba(237, 194, 46, 0.87)"];
+  // color = getRandomInt(0, 2);
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+  var date = undefined;
+  if (time) date = new Date(time);
   
-      var str = `<div class="card card-md" style="background: ${color}; aspect-ratio: 16 / 9; width: fit-content;">
-          <div class="row g-0 upper-part">
-            <div class="col-md-6">
-              <img src="${ image_path ? '/images/posts/' + image_path : '/images/wave.jpg'}" class="card-img" alt="..." />
-            </div>
-            <div class="col-md-6 full-height">
-              <div class="card-body">
-                <p class="card-text">${message}</p>
+  var str = `<div class="card ${size}" style="background: white; ">
+        <div class="msg-body">
+          <div class="msg-half">
+            <img src="/images/posts/${filename}" class="msg-img rounded" alt="..." />
+          </div>
+          <div class="msg-half msg-text"><div id=${id} class="msg" style="vertical-align: middle; ">
+              ${message}
               </div>
-            </div>
           </div>
-          <div class="card-footer lower-part">
-            <i class="material-icons" style="font-size: 1.5vh">
-              account_circle</i
-            >
-            ${nickname}
-            <i class="material-icons" style="font-size: 1.5vh"> mail</i>
-            ${email}
+        </div>
+        <div style="height:4.5%;border-bottom: 0.5px rgba(76, 76, 76, 0.492) solid; box-shadow: 0 1.2px 1px -1px rgba(104, 104, 104, 0.444);"></div>
+        <div style="height:2.5%"></div>
+        <div class="msg-footer">
+          <div style="float: left; height: 100%"> 
+          ${nickname ? `<i class="material-icons">account_circle</i><span style="vertical-align:middle;">   ${nickname}</span>` : ""}
           </div>
-        </div>`;
+          <div style="float: right; height: 100%">
+          ${date ? `<i class="material-icons"> access_time</i><span style="vertical-align:middle">   ${monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()}</span>` : ""}
+          </div>
+        </div>
+      </div>`;
 
-        var str_without_img = `<div class="card card-md" style="background: ${color}; aspect-ratio: 16 / 9; width: fit-content;">
-                    <div class="row g-0 upper-part">
-                    <div class="col-md-12 full-height">
-                        <div class="card-body">
-                        <p class="card-text">${message}</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="card-footer lower-part">
-                    <i class="material-icons" style="font-size: 1.5vh">
-                        account_circle</i
-                    >
-                    ${nickname}
-                    <i class="material-icons" style="font-size: 1.5vh"> mail</i>
-                    ${email}
-                    </div>
-                </div>`;
+  var str_without_img = `<div class="card ${size}" style="background: white;">
+    <div class="msg-body">
+      <div class="msg-text" style="padding: 0 0 0 0">
+        <div class="msg" id=${id} style="vertical-align: middle;">
+          ${message}
+        </div>
+      </div>
+    </div>
+    <div style="height:4.5%;border-bottom: 0.5px rgba(76, 76, 76, 0.492) solid; box-shadow: 0 1.2px 1px -1px rgba(104, 104, 104, 0.444);"></div>
+    <div style="height:2.5%"></div>
+    <div class="msg-footer">
+      <div style="float: left; height: 100%;"> 
+      ${nickname ? `<i class="material-icons">account_circle</i><span style="vertical-align:middle">${nickname}</span>` : ""}
+      </div>
+      <div style="float: right; height: 100%">
+      ${date ? `<i class="material-icons"> access_time</i><span style="vertical-align:middle">${monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()}</span>` : ""}
+    </div>
+  </div>`;
 
-      var parser = new DOMParser();
-      var container = document.getElementById("modal-content");
-      container.innerHTML = '';
-      console.log("test", image_path);
-      if(image_path) {
-        var temp = parser.parseFromString(str, "text/html");
-      } else {
-        var temp = parser.parseFromString(str_without_img, "text/html");  
-      } 
-      
-
-      return [container.appendChild(temp.body.firstChild), cur];
-  }
-
-// import postAPI from "./api/postAPI.js";
-
-// console.log(postAPI.getItems(1));
-
-// var boards = [b1posts,b2posts]
-
-// function showPost(posts) {
-
-// }
-
-// $.each(boards,function(posts){
-//     $.each(posts,function(){
-        
-//     });
-    
-// });
-
-// document.getElementById("reject").onclick = function(){
-//     post["status"] = 2;
-//     console.log(post);
-// }
-
-// document.getElementById("approve").onclick = function(){
-//     post["status"] = 1;
-//     console.log(post);
-// }
-
+  var str_without_text = `<div class="card ${size}" style="background: white;">
+  <div class="msg-body">
+    <div class="msg-image">
+      <img src="/images/posts/${filename}" class="msg-img" alt="..." />
+    </div>
+  </div>
+  <div style="height:4.5%;border-bottom: 0.5px rgba(76, 76, 76, 0.492) solid; box-shadow: 0 1.2px 1px -1px rgba(104, 104, 104, 0.444);"></div>
+    <div style="height:2.5%"></div>
+    <div class="msg-footer">
+      <div style="float: left; height: 100%"> 
+      ${nickname ? `<i class="material-icons">account_circle</i><span style="vertical-align:middle;">${nickname}</span>` : ""}
+      </div>
+      <div style="float: right; height: 100%">
+      ${date ? `<i class="material-icons"> access_time</i><span style="vertical-align:middle">${monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()}</span>` : ""}
+    </div>
+</div>`;
+var parser = new DOMParser();
+var container = document.getElementById("modal-content");
+container.innerHTML = '';
+if(filename) {
+  var temp = parser.parseFromString(str, "text/html");
+} else {
+  var temp = parser.parseFromString(str_without_img, "text/html");  
+} 
+  return [container.appendChild(temp.body.firstChild)];
+}
